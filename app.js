72f4ecbac6a4574ddb3a8c9fe4932b05f6922f03
ravinder.gugloth <ravinder.gugloth@gmail.com>
2024-06-30 09:48:21 +0100

    files added

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log("Today is holiday")
+console.log("Ravinder is handsome")
+
+
+
+
+//Arithmetic operators
+let x=10;
+let y=5;
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
+console.log(x**y);
+console.log(x%y);
+console.log(x++)
+console.log(++x)
+
+
+//Practi e question
+let color="green"
+if (color=="Red")
+    {
+    console.log("stop")
+    } 
+if(color=="green")
+    {
+    console.log("drive safely")
+    }
+if(color=="yellow") 
+    {
+     console.log("slow down")
+    }
+
+    //Program
+    let str="apple"
+    if((str[0] === "a") && (str.length>=3)) {
+        console.log("good string")
+    } else {
+        console.log("not a good string")
+    }
+
+    let msg="   Hello    "
+    console.log(msg)
